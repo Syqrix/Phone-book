149ee5afe42be096841864f348b044bbebc22192
Edward Mironov <edwardmir0515@gmail.com>
2025-09-18 17:17:20 +0500

    save point(working)

---
+++ phone-book.py
@@ -0,0 +1,85 @@
+class PhoneBook:
+    def __init__(self):
+        self.phone_book = []
+
+    def say_hi(self):
+        print("Welcome! This is phone book app! You can press q to quit.")
+
+    def check_contacts(self):
+        for i in self.phone_book:
+            print(i)
+
+    def add_contact(self):
+        while True:
+            name = input("Name: ")
+            if not name:
+                print("Please enter the name.")
+                continue
+            if name.isdigit():
+                print("Please enter the letters!")
+                continue
+            else:
+                break
+        name = name
+        while True:
+            try:
+                phone = int(input("Please enter your number only 11 numbers"))
+            except TypeError:
+                print("Wrong type again!")
+                continue
+            if len(str(phone)) != 11:
+                print("Number more or less than 11. Please try again")
+                continue
+            else:
+                break
+        phone = "+" + str(phone)
+        self.phone_book.append({"name": {name},
+                                "phone": {phone}})
+
+    def edit_contatct(self):
+        pass
+
+    def delete_contact(self):
+        pass
+
+    def clear_phone_book(self):
+        pass
+
+    def user_wish(self):
+        operations = {1: self.check_contacts,
+                      2: self.add_contact,
+                      3: self.edit_contatct,
+                      4: self.delete_contact,
+                      5: self.clear_phone_book,
+                      }
+        print("\n Avaibal operations:")
+        for keys, func in operations.items():
+            print(f"{keys}: {func.__name__}")
+        while True:
+            user_answer_str = input(
+                "What kind of operation do you want to use? ")
+            if not user_answer_str:
+                print("It's empty please try enter something!")
+                continue
+            if user_answer_str.lower() == "q":
+                return None
+            if user_answer_str.isdigit():
+                user_answer = int(user_answer_str)
+                operations[user_answer]()
+            else:
+                print("Only numbers!")
+                continue
+
+
+def main():
+    while True:
+        app = PhoneBook()
+        app.say_hi()
+        answer = app.user_wish()
+        if answer is None:
+            print("End of the programm")
+            break
+
+
+if __name__ == "__main__":
+    main()
